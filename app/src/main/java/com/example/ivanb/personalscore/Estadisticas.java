3eee045ab66423c5d6a4ddd3e1e70c5497d4c21c
Iván Bruna García <ivan.bruna.garcia@gmail.com>
2016-03-11 11:59:06 +0100

    Comenzamos modo JUGADAS

--- app/src/main/java/com/example/ivanb/personalscore/Estadisticas.java
+++ app/src/main/java/com/example/ivanb/personalscore/Estadisticas.java
@@ -4,6 +4,7 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.TextView;
@@ -34,6 +35,8 @@ public class Estadisticas extends AppCompatActivity implements View.OnClickListe
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.estadisticas);
 
         /* CARGAR LOS TV CON LOS DATOS DEL LAYOUT */
--- app/src/main/java/com/example/ivanb/personalscore/MenuInicial.java
+++ app/src/main/java/com/example/ivanb/personalscore/MenuInicial.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageButton;
 
 public class MenuInicial extends AppCompatActivity implements View.OnClickListener {
@@ -11,6 +12,8 @@ public class MenuInicial extends AppCompatActivity implements View.OnClickListen
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.menu_principal);
         siguiente();
     }

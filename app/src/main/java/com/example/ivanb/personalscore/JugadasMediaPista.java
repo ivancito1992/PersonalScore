81b4a2a692001f8c0aae884d71be43d9ae42a825
Iván Bruna García <ivan.bruna.garcia@gmail.com>
2016-03-16 14:02:14 +0100

    comenzamos con el CANVAS CDZ POWAH

--- app/src/main/java/com/example/ivanb/personalscore/JugadasMediaPista.java
+++ app/src/main/java/com/example/ivanb/personalscore/JugadasMediaPista.java
@@ -1,13 +1,24 @@
 package com.example.ivanb.personalscore;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.AttributeSet;
+import android.view.View;
 
-public class JugadasMediaPista extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+public class JugadasMediaPista extends View {
+    
+    public JugadasMediaPista(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setupDrawing();
+    }
+
+    private void setupDrawing() {
+    }
+
+   /*protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.jugadas_media_pista);
-    }
+    }*/
 }

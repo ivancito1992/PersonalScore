afd88428f23385dc662595af13710838ef9ac6c8
Iván Bruna García <ivan.bruna.garcia@gmail.com>
2016-03-17 12:03:27 +0100

    YA PINTAAAAA

--- app/src/main/java/com/example/ivanb/personalscore/JugadasMediaPista.java
+++ app/src/main/java/com/example/ivanb/personalscore/JugadasMediaPista.java
@@ -1,24 +1,26 @@
 package com.example.ivanb.personalscore;
 
-
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.AttributeSet;
 import android.view.View;
+import android.widget.Button;
 
-public class JugadasMediaPista extends View {
-    
-    public JugadasMediaPista(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        setupDrawing();
+
+public class JugadasMediaPista extends AppCompatActivity implements View.OnClickListener{
+
+    Button borrar;
+   protected void onCreate(Bundle savedInstanceState) {
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.jugadas_media_pista);
+
+       borrar = (Button) findViewById(R.id.borrar);
+   }
+
+    @Override
+    public void onClick(View v) {
+        Pintar pintar = new Pintar(this);
+        if(v.getId()==R.id.borrar){
+            pintar.BorrarCanvas();
+        }
     }
-
-    private void setupDrawing() {
-    }
-
-   /*protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.jugadas_media_pista);
-    }*/
 }
